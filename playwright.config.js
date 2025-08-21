ef0164fb797c1cbb9d8f3bbeaf39a21cd894df62
Abhinav Singh <abhinav.singh@gspann.com>
2025-08-22 00:26:47 +0530

    Added the chekout functionality

--- playwright.config.js
+++ playwright.config.js
@@ -21,19 +21,22 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    
     trace: 'on-first-retry',
     channel: 'chrome',
-    slowMo: 5000,
+    
   },
+  
 
   /* Configure projects for major browsers */
   projects: [
@@ -80,4 +83,3 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
